9b5d05f5cdc50afd0e4ce04ec7dc4608cbe1d621
iiroalhonen <iiro.alhonen@kpsbremen.de>
2021-04-14 11:39:25 +0200

    Initial Commit

---
+++ Color Replacement/Color_ReplacementApp.swift
@@ -0,0 +1,17 @@
+//
+//  Color_ReplacementApp.swift
+//  Color Replacement
+//
+//  Created by Iiro Alhonen on 14.04.21.
+//
+
+import SwiftUI
+
+@main
+struct Color_ReplacementApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
